161eee2e88449187cac9a0e0fefde37ae270586a
Abhinav Arora <132556535+abhinav020104@users.noreply.github.com>
2024-03-03 08:06:50 +0530

    Delete outDir/server directory

--- outDir/server/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-app.listen(4000, () => {
-    console.log("server listening at port 4000");
-});
